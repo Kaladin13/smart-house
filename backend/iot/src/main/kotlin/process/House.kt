e9f701e4c9c42913bd5d29d8e4658f05a8946ee4
bakalover <kostya@shprenger.com>
2024-05-21 13:07:49 +0300

    Light and climate

--- backend/iot/src/main/kotlin/process/House.kt
+++ backend/iot/src/main/kotlin/process/House.kt
@@ -8,16 +8,34 @@ import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.launch
 import org.bakalover.iot.message.Request
 import org.bakalover.iot.message.Response
+import org.bakalover.iot.message.ResponseCode
 import org.bakalover.iot.pipeline.ISwitch
+import org.bakalover.iot.things.ClimateControl
+import org.bakalover.iot.things.IThing
+import org.bakalover.iot.things.Light
 
 class House(private val id: Int) : IIntermediateProcess<Request, Response> {
     private lateinit var receiveChannel: ReceiveChannel<Request>
     private lateinit var sendChannel: SendChannel<Response>
     private lateinit var job: Job
+    private lateinit var things: Map<String, IThing>;
 
     override fun doProcess(commutationFrom: ISwitch<Request>, commutationTo: ISwitch<Response>, scope: CoroutineScope) {
         receiveChannel = commutationFrom.getChannel(id)
         sendChannel = commutationTo.getChannel(id)
+
+        val climate = ClimateControl()
+        val light = Light()
+
+        climate.backgroundActivity(scope)
+        light.backgroundActivity(scope)
+
+        things = mapOf(
+            Pair("climate", climate),
+            Pair("light", light)
+        )
+
+
         job = scope.launch {
             run()
         }
@@ -26,19 +44,41 @@ class House(private val id: Int) : IIntermediateProcess<Request, Response> {
     private suspend fun run() {
         while (true) {
             processRequest()
-            sendUpdate()
         }
     }
 
     private suspend fun processRequest() {
-        receiveChannel.receive()
-    }
+        val request = receiveChannel.receive()
+        val thing = request.thing
+        if (things.contains(thing)) {
+            val thingResponse = things[thing]?.doAction(request.action)
+            if (thingResponse != null) {
+                sendChannel.send(
+                    Response(
+                        request.taskId,
+                        request.houseId,
+                        thingResponse.first,
+                        thingResponse.second
+                    )
+                )
+            } else {
+                sendChannel.send(
+                    Response(
+                        request.taskId,
+                        request.houseId,
+                        ResponseCode.INTERNAL_ERROR,
+                        ""
+                    )
+                )
 
-    private suspend fun sendUpdate() {
-        sendChannel.send(Response(1, "hjjjjjjjjjjjjjjjjjjji"))
+            }
+        } else {
+            sendChannel.send(Response(request.taskId, request.houseId, ResponseCode.THING_DOES_NOT_EXISTS, ""))
+        }
     }
 
     override suspend fun abort() {
+        things.forEach { (_, thing) -> thing.abort() }
         job.cancelAndJoin()
     }
 }
--- backend/iot/src/main/kotlin/things/ClimateControl.kt
+++ backend/iot/src/main/kotlin/things/ClimateControl.kt
@@ -1,6 +1,88 @@
 package org.bakalover.iot.things
 
-class ClimateControl(private var t: Int) {
+import kotlinx.coroutines.*
+import org.bakalover.iot.message.ResponseCode
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+
+const val lowerBoundTemperature = 18
+const val upperBoundTemperature = 40
+const val lowerBoundHumidity = 0
+const val upperBoundHumidity = 100
+
+class ClimateControl() : IThing {
+    private var t = AtomicInteger(20)
+    private var h = AtomicInteger(50) // Percentage
+    private lateinit var job: Job;
+
+    override fun doAction(action: String): Pair<ResponseCode, String> {
+        val parts = action.split(" ")
+
+        if (parts.size != 3) {
+            return Pair(ResponseCode.INVALID_ACTION_FORMAT, "")
+        }
+
+        when (parts[0]) {
+            "set" -> {
+                when (parts[1]) {
+                    "t" -> {
+                        try {
+                            val to = parts[2].toInt()
+                            if (to > upperBoundTemperature || to < lowerBoundTemperature) {
+                                return Pair(ResponseCode.INVALID_TEMPERATURE, "")
+                            }
+                            t.set(to)
+                            return Pair(ResponseCode.OK, "")
+                        } catch (e: NumberFormatException) {
+                            return Pair(ResponseCode.INVALID_ACTION_FORMAT, "")
+                        }
+                    }
+
+                    "h" -> {
+                        try {
+                            val to = parts[2].toInt()
+                            if (to > upperBoundHumidity || to < lowerBoundHumidity) {
+                                return Pair(ResponseCode.INVALID_HUMIDITY, "")
+                            }
+                            h.set(to)
+                            return Pair(ResponseCode.OK, "")
+                        } catch (e: NumberFormatException) {
+                            return Pair(ResponseCode.INVALID_ACTION_FORMAT, "")
+                        }
+                    }
+
+                    else -> {
+                        return Pair(ResponseCode.INVALID_ACTION_FORMAT, "")
+                    }
+                }
+            }
+
+            "get" -> {
+                return Pair(ResponseCode.OK, "$t $h")
+            }
+
+            else -> {
+                return Pair(ResponseCode.INVALID_ACTION_FORMAT, "")
+            }
+        }
+
+    }
+
+    override fun backgroundActivity(scope: CoroutineScope) {
+        job = scope.launch {
+            while (true) {
+                val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+                if (currentHour > 20) {
+                    t.set(19)
+                }
+                delay(1000 * 60 * 10) // 10 minutes
+            }
+        }
+    }
+
+    override suspend fun abort() {
+        job.cancelAndJoin()
+    }
 }
 
 
