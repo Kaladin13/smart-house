62e7d6e5462468448eb63735d07b39908f79b24f
bakalover <kostya@shprenger.com>
2024-05-21 13:07:37 +0300

    Protocol

--- backend/iot/src/main/kotlin/message/Response.kt
+++ backend/iot/src/main/kotlin/message/Response.kt
@@ -3,4 +3,9 @@ package org.bakalover.iot.message
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Response(val houseId: Int, val report: String)
+data class Response(
+    val taskId: Int,
+    val houseId: Int,
+    val responseCode: ResponseCode,
+    val report: String
+)
---
+++ backend/iot/src/main/kotlin/message/ResponseCode.kt
@@ -0,0 +1,10 @@
+package org.bakalover.iot.message
+
+enum class ResponseCode(val code: ULong) {
+    OK(0u),
+    THING_DOES_NOT_EXISTS(101u),
+    INVALID_ACTION_FORMAT(102u),
+    INVALID_TEMPERATURE(103u),
+    INVALID_HUMIDITY(104u),
+    INTERNAL_ERROR(105u),
+}
